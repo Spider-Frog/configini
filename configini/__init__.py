a1788c13be0f358702436bcad05f9cf822e1f9c7
spiderfrog <germanspiderfrog@gmail.com>
2020-12-31 00:34:34 +0100

    configini V0.2
    Added data type unit test
    Implemented all functions

---
+++ configini/__init__.py
@@ -0,0 +1,47 @@
+import configparser
+import os
+import re
+
+
+__config = configparser.ConfigParser()
+
+
+def read(file):
+    __config.read(file)
+
+
+def get(chapter, key, default=None, data_type=str, environment_var=None):
+    """
+    Function to retrieve the value from the configurations file.
+
+    :param str chapter: Name of the chapter inside the configurations file.
+    :param str key: variable name inside the chapter.
+    :param default: Value to default to, when no value is given.
+    :param data_type: Data type the value should be cast to.
+    :param str environment_var: Overwrites the chapter_key value.
+    :return: The value from the configurations value after being parsed.
+    """
+    try:
+        value = os.environ.get(environment_var or f"{chapter.upper()}_{key.upper()}") or \
+                   __config[chapter.lower()][key.lower()] or \
+                   default
+
+        # If value is None just immediately return the default.
+        if value is None:
+            return default
+
+        # Else if value contains a dot, and needs to be cast as an integer.
+        # Cast it to float first, then integer to prevent an exception.
+        elif data_type == int and '.' in value:
+            return int(float(value))
+
+        # Else if value is 0 or false and needs to be cast to a boolean.
+        # then just immediately return False
+        elif data_type == bool and value in ('0', 'false'):
+            return False
+
+        # Cast value to right data type.
+        return data_type(value)
+    except KeyError:
+        # Return the default value.
+        return default
--- tests/test_data_types.py
+++ tests/test_data_types.py
@@ -1,20 +1,167 @@
 import unittest
+import configini
 
 
-class TestDataTypes(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+class TestString(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        configini.read('config_data_types.ini')
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+        class Config:
+            string_text = configini.get('string', 'text')
+            string_integer = configini.get('string', 'integer')
+            string_double_quotes = configini.get('string', 'double_quotes')
+            string_single_quotes = configini.get('string', 'single_quotes')
+            none_no_text = configini.get('string', 'no_text')
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+        cls.config = Config
+
+    def test_str_text(self):
+        self.assertEqual(type(self.config.string_text), str)
+
+    def test_str_integer(self):
+        self.assertEqual(type(self.config.string_integer), str)
+
+    def test_str_double_quotes(self):
+        self.assertEqual(type(self.config.string_double_quotes), str)
+
+    def test_str_single_quotes(self):
+        self.assertEqual(type(self.config.string_single_quotes), str)
+
+    def test_none_no_text(self):
+        self.assertIsNone(self.config.none_no_text)
+
+
+class TestInteger(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        configini.read('config_data_types.ini')
+
+        class Config:
+            zero = configini.get('number', 'zero', data_type=int)
+            one = configini.get('number', 'one', data_type=int)
+            negative_one = configini.get('number', 'negative_one', data_type=int)
+            zero_with_dot = configini.get('number', 'zero_with_dot', data_type=int)
+            one_with_dot = configini.get('number', 'one_with_dot', data_type=int)
+            negative_one_with_dot = configini.get('number', 'negative_one_with_dot', data_type=int)
+            one_dot_five = configini.get('number', 'one_dot_five', data_type=int)
+
+        cls.config = Config
+
+    def test_integer_zero(self):
+        self.assertEqual(type(self.config.zero), int)
+
+    def test_integer_one(self):
+        self.assertEqual(type(self.config.one), int)
+
+    def test_integer_negative_one(self):
+        self.assertEqual(type(self.config.negative_one), int)
+
+    def test_integer_zero_with_dot(self):
+        self.assertEqual(type(self.config.zero_with_dot), int)
+
+    def test_integer_one_with_dot(self):
+        self.assertEqual(type(self.config.one_with_dot), int)
+
+    def test_integer_negative_one_with_dot(self):
+        self.assertEqual(type(self.config.negative_one_with_dot), int)
+
+    def test_integer_one_dot_five(self):
+        self.assertEqual(type(self.config.one_dot_five), int)
+
+
+class TestFloat(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        configini.read('config_data_types.ini')
+
+        class Config:
+            zero = configini.get('number', 'zero', data_type=float)
+            one = configini.get('number', 'one', data_type=float)
+            negative_one = configini.get('number', 'negative_one', data_type=float)
+            zero_with_dot = configini.get('number', 'zero_with_dot', data_type=float)
+            one_with_dot = configini.get('number', 'one_with_dot', data_type=float)
+            negative_one_with_dot = configini.get('number', 'negative_one_with_dot', data_type=float)
+            one_dot_five = configini.get('number', 'one_dot_five', data_type=float)
+
+        cls.config = Config
+
+    def test_integer_zero(self):
+        self.assertEqual(type(self.config.zero), float)
+
+    def test_integer_one(self):
+        self.assertEqual(type(self.config.one), float)
+
+    def test_integer_negative_one(self):
+        self.assertEqual(type(self.config.negative_one), float)
+
+    def test_integer_zero_with_dot(self):
+        self.assertEqual(type(self.config.zero_with_dot), float)
+
+    def test_integer_one_with_dot(self):
+        self.assertEqual(type(self.config.one_with_dot), float)
+
+    def test_integer_negative_one_with_dot(self):
+        self.assertEqual(type(self.config.negative_one_with_dot), float)
+
+    def test_integer_one_dot_five(self):
+        self.assertEqual(type(self.config.one_dot_five), float)
+
+
+class TestBool(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        configini.read('config_data_types.ini')
+
+        class Config:
+            bool_with_1 = configini.get('bool', '1', data_type=bool)
+            bool_with_true = configini.get('bool', 'true', data_type=bool)
+            bool_with_text = configini.get('bool', 'text', data_type=bool)
+            bool_with_0 = configini.get('bool', '0', data_type=bool)
+            bool_with_false = configini.get('bool', 'false', data_type=bool)
+
+        cls.config = Config
+
+    def test_bool_with_1(self):
+        self.assertTrue(self.config.bool_with_1)
+
+    def test_bool_with_true(self):
+        self.assertTrue(self.config.bool_with_true)
+
+    def test_bool_with_text(self):
+        self.assertTrue(self.config.bool_with_text)
+
+    def test_bool_with_0(self):
+        self.assertFalse(self.config.bool_with_0)
+
+    def test_bool_with_false(self):
+        self.assertFalse(self.config.bool_with_false)
+
+
+class TestNone(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        configini.read('config_data_types.ini')
+
+        class Config:
+            none_string = configini.get('none', 'none', data_type=str)
+            none_integer = configini.get('none', 'none', data_type=int)
+            none_float = configini.get('none', 'none', data_type=float)
+            none_bool = configini.get('none', 'none', data_type=bool)
+
+        cls.config = Config
+
+    def test_none_string(self):
+        self.assertIsNone(self.config.none_string)
+
+    def test_none_integer(self):
+        self.assertIsNone(self.config.none_integer)
+
+    def test_none_float(self):
+        self.assertIsNone(self.config.none_float)
+
+    def test_nnone_bool(self):
+        self.assertIsNone(self.config.none_bool)
 
 
 if __name__ == '__main__':
